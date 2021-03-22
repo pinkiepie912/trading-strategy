a2769a43037ffb4b63f2feb835fc94268d040aec
Hyun <pinkiepies0912@gmail.com>
2021-03-23 01:01:32 +0900

    Add Analysis

---
+++ trading_strategy/analysis.py
@@ -0,0 +1,65 @@
+from functools import cache, cached_property
+
+import numpy as np
+import pandas as pd
+
+from .constants import TRADING_DAYS_OF_A_YEAR
+from .models import PriceHistory
+
+
+class Analysis:
+    DAILY_RETURN = "daily_rtn"
+    ST_RETURN = "st_rtn"
+
+    def __init__(self, history: PriceHistory):
+        super().__init__()
+        self._history = history
+
+    @cached_property
+    def df(self) -> pd.DataFrame:
+        _df = self._history.to_df()
+        _df[self.DAILY_RETURN] = _df.adjclose.pct_change()  # with daily return
+        _df[self.ST_RETURN] = (
+            1 + _df[self.DAILY_RETURN]
+        ).cumprod()  # with st return
+        return _df
+
+    def cagr(self) -> float:
+        """
+        Compound Annual Growth Rate
+        """
+        return (
+            self.df.iloc[-1][self.ST_RETURN]
+            ** (TRADING_DAYS_OF_A_YEAR / len(self.df.index))
+            - 1
+        )
+
+    @cache
+    def vol(self):
+        """
+        Volatility
+        """
+        return np.std(self.df[self.DAILY_RETURN]) * np.sqrt(
+            TRADING_DAYS_OF_A_YEAR
+        )
+
+    def sharp(self):
+        """
+        Ex-post Sharpe ratio
+        """
+        return (
+            np.mean(self.df[self.DAILY_RETURN])
+            / np.std(self.df[self.DAILY_RETURN])
+            * np.sqrt(TRADING_DAYS_OF_A_YEAR)
+        )
+
+    def mdd(self) -> float:
+        return self._historical_mdd().min()
+
+    def _historical_mdd(self) -> pd.DataFrame:
+        """
+        Maximum Draw Down
+        """
+        historical_max = self.df.adjclose.cummax()
+        daily_drawdown = self.df.adjclose / historical_max - 1
+        return daily_drawdown.cummin()
--- trading_strategy/constants.py
+++ trading_strategy/constants.py
@@ -1,1 +1,11 @@
+"""
+trading_strategy.constants
+
+"""
+
+# Date
 DATE_FMT = "%Y-%m-%d"
+
+# Business days
+# TODO: 편의상 상수로 두었으나, 연별 계산 해야함
+TRADING_DAYS_OF_A_YEAR = 252
