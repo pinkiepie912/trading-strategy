3390e6661643ded372fb3bb471862e3ac098c50e
Hyun <pinkiepies0912@gmail.com>
2021-03-22 21:47:12 +0900

    Use models in package

--- trading_strategy/clients/yf.py
+++ trading_strategy/clients/yf.py
@@ -2,12 +2,11 @@ from __future__ import annotations
 
 import datetime
 import enum
-from dataclasses import dataclass
-from typing import List
 
 from yahoofinancials import YahooFinancials
 
-from ..constants import DATE_FMT
+from ..models import Currency, PriceHistory
+from ..utils import date_to_str
 from .exceptions import (
     InvalidPeriodError,
     InvalidTickerError,
@@ -19,11 +18,6 @@ class Interval(enum.Enum):
     DAILY = "daily"
     WEEKLY = "weekly"
     MONTHLY = "monthly"
-
-
-class Currency(enum.Enum):
-    USD = "USD"
-    KRW = "KRW"
 
 
 # TODO: 개발 편의상 client를 두었으나, trading repo로 이전해야함.
@@ -38,54 +32,23 @@ class YahooFinanceClient:
         start_date: datetime.date,
         end_date: datetime.date,
         time_interval: Interval,
-    ) -> List[Price]:
+    ) -> PriceHistory:
         if start_date > end_date:
             raise InvalidPeriodError
 
         yahoo_financial = YahooFinancials(ticker)
-        history = yahoo_financial.get_historical_price_data(
-            start_date=self._date_to_str(start_date),
-            end_date=self._date_to_str(end_date),
+        data = yahoo_financial.get_historical_price_data(
+            start_date=date_to_str(start_date),
+            end_date=date_to_str(end_date),
             time_interval=time_interval.value,
         )
 
-        if "prices" not in history[ticker]:
+        data = data[ticker]
+        if "prices" not in data:
             raise InvalidTickerError
 
-        prices = history[ticker]["prices"]
+        prices = data["prices"]
         if not prices:
             raise NoHistoricalDataError
 
-        currency = Currency(history[ticker]["currency"])
-
-        return [
-            Price(
-                adjclose=price["adjclose"],
-                close=price["close"],
-                date=self._str_to_date(price["formatted_date"]),
-                high=price["high"],
-                low=price["low"],
-                open=price["open"],
-                volume=price["volume"],
-                currency=currency,
-            )
-            for price in prices
-        ]
-
-    def _date_to_str(self, date: datetime.date, fmt=DATE_FMT) -> str:
-        return date.strftime(fmt)
-
-    def _str_to_date(self, str_date: str, fmt=DATE_FMT) -> datetime.date:
-        return datetime.datetime.strptime(str_date, fmt).date()
-
-
-@dataclass
-class Price:
-    adjclose: float
-    close: float
-    date: datetime.date
-    high: float
-    low: float
-    open: float
-    volume: int
-    currency: Currency
+        return PriceHistory.of(prices, Currency(data["currency"]))
